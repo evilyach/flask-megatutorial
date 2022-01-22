c3f03abbbc05eb9b7d2952a27d6fd31a1b38fba8
Ilya Chesalin <ilya.chesalin@gmail.com>
2022-01-22 17:50:09 +0300

    Configure app to use SQLAlchemy and Alembic (migrate)

--- config.py
+++ config.py
@@ -1,4 +1,10 @@
 import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY") or "you-shall-never-guess-this"
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URI") or \
+        f"sqlite:///{os.path.join(basedir, 'app.db')}"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
