ad999333584947e05ba4986c64d435496a32795a
Ilya Chesalin <ilya.chesalin@gmail.com>
2022-01-23 02:35:31 +0300

    Add login to flask shell context

--- microblog.py
+++ microblog.py
@@ -1,8 +1,13 @@
-from app import app, db
+from app import (
+    app,
+    db,
+    login
+)
 
 
 @app.shell_context_processor
 def make_shell_context():
     return {
-        "db": db
+        "db": db,
+        "login": login,
     }
