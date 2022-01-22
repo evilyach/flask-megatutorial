df7abc83ca01040ddd7933bc664c0c6faa68768d
Ilya Chesalin <ilya.chesalin@gmail.com>
2022-01-23 02:29:44 +0300

    Require users to login

--- app/routes.py
+++ app/routes.py
@@ -2,13 +2,16 @@ from flask import (
     flash,
     redirect,
     render_template,
+    request,
     url_for,
 )
 from flask_login import (
     current_user,
+    login_required,
     login_user,
     logout_user,
 )
+from werkzeug.urls import url_parse
 
 from app import app
 from app.forms import LoginForm
@@ -17,6 +20,7 @@ from app.models import User
 
 @app.route("/")
 @app.route("/index")
+@login_required
 def index():
     user = {"username": "Ilya"}
     posts = [
@@ -51,7 +55,12 @@ def login():
             return redirect(url_for("login"))
 
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for("index"))
+
+        next = request.args.get("next")
+        if not next or url_parse(next).netloc != "":
+            next = url_for("index")
+
+        return redirect(next)
 
     return render_template(
         "login.html",
