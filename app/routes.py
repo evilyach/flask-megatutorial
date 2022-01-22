8802771b12af578072f0e9b0506a4e3461128250
Ilya Chesalin <ilya.chesalin@gmail.com>
2022-01-22 17:29:53 +0300

    Add POST request handler for login page

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,8 @@
-from flask import render_template
+from flask import (
+    flash,
+    redirect,
+    render_template,
+)
 from app import app
 from app.forms import LoginForm
 
@@ -25,9 +29,16 @@ def index():
     )
 
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
+
+    if form.validate_on_submit():
+        flash("Login requested for user = {}, remember_me = {}".format(
+            form.username.data, form.remember_me.data
+        ))
+
+        return redirect("/index")
 
     return render_template(
         "login.html",
