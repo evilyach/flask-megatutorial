eb3529fa21f95202d367b312db1f84c3c31f9cb0
Ilya Chesalin <ilya.chesalin@gmail.com>
2022-01-23 16:56:41 +0300

    Refactor logging

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,3 @@
-import logging
-from logging.handlers import (
-    RotatingFileHandler,
-    SMTPHandler,
-)
-import os
-
 from flask import Flask
 from flask_login import LoginManager
 from flask_migrate import Migrate
@@ -22,42 +15,4 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = "login"
 
-from app import routes, models, errors
-
-
-if not app.debug and app.config["MAIL_SERVER"]:
-    auth = None
-    if app.config["MAIL_USERNAME"] and app.config["MAIL_PASSWORD"]:
-        auth = (app.config["MAIL_USERNAME"], app.config["MAIL_PASSWORD"])
-
-    secure = None
-    if app.config["MAIL_USE_TLS"]:
-        secure = ()
-
-    mail_handler = SMTPHandler(
-        mailhost=(app.config["MAIL_SERVER"], app.config["MAIL_PORT"]),
-        fromaddr=f"no-reply@{app.config['MAIL_SERVER']}",
-        toaddrs=app.config["ADMINS"],
-        subject="Microblog Failure",
-        credentials=auth,
-        secure=secure
-    )
-    mail_handler.setLevel(logging.ERROR)
-
-    app.logger.addHandler(mail_handler)
-
-if not app.debug:
-    if not os.path.exists("logs"):
-        os.mkdir("logs")
-
-    file_handler = RotatingFileHandler(
-        "logs/microblog.log",
-        maxBytes=10240,
-        backupCount=10
-    )
-    file_handler.setLevel(logging.INFO)
-    
-    app.logger.addHandler(file_handler)
-
-    app.logger.setLevel(logging.INFO)
-    app.logger.info("Microblog startup")
+from app import routes, models, errors, log
---
+++ app/log.py
@@ -0,0 +1,52 @@
+import logging
+from logging.handlers import (
+    RotatingFileHandler,
+    SMTPHandler,
+)
+import os
+
+from app import app
+
+
+def enable_logging_to_mail():
+    auth = None
+    if app.config["MAIL_USERNAME"] and app.config["MAIL_PASSWORD"]:
+        auth = (app.config["MAIL_USERNAME"], app.config["MAIL_PASSWORD"])
+
+    secure = None
+    if app.config["MAIL_USE_TLS"]:
+        secure = ()
+
+    mail_handler = SMTPHandler(
+        mailhost=(app.config["MAIL_SERVER"], app.config["MAIL_PORT"]),
+        fromaddr=f"no-reply@{app.config['MAIL_SERVER']}",
+        toaddrs=app.config["ADMINS"],
+        subject="Microblog Failure",
+        credentials=auth,
+        secure=secure
+    )
+    mail_handler.setLevel(logging.ERROR)
+
+    app.logger.addHandler(mail_handler)
+
+def enable_logging_to_file():
+    if not os.path.exists("logs"):
+        os.mkdir("logs")
+
+    file_handler = RotatingFileHandler(
+        "logs/microblog.log",
+        maxBytes=10240,
+        backupCount=10
+    )
+    file_handler.setLevel(logging.INFO)
+    
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info("Microblog startup")
+
+
+if not app.debug:
+    if app.config["MAIL_SERVER"]:
+        enable_logging_to_mail()
+    enable_logging_to_file()
