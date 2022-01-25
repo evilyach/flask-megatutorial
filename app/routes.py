e7bc767320409b51b26d29b2408c7ffaa332a73a
Ilya Chesalin <ilya.chesalin@gmail.com>
2022-01-25 19:43:46 +0300

    Add page pagination for user posts in index and explore
    See #1

--- app/routes.py
+++ app/routes.py
@@ -54,21 +54,37 @@ def index():
         flash("Your post is now live!")
         return redirect(url_for("index"))
 
-    posts = current_user.followed_posts().all()
+    page = request.args.get("page", default=1, type=int)
+
+    posts = current_user.followed_posts().paginate(
+        page, app.config["POSTS_PER_PAGE"], False
+    )
 
     return render_template(
         "index.html",
         title="Home",
         form=form,
-        posts=posts
+        posts=posts.items,
+        next_url=url_for("index", page=posts.next_num) if posts.has_next else None,
+        prev_url=url_for("index", page=posts.prev_num) if posts.has_prev else None,
     )
 
 
 @app.route("/explore")
 @login_required
 def explore():
-    posts = Post.query.order_by(Post.timestamp.desc()).all()
-    return render_template("index.html", title="Explore", posts=posts)
+    page = request.args.get("page", default=1, type=int)
+    posts = Post.query.order_by(Post.timestamp.desc()).paginate(
+        page, app.config["POSTS_PER_PAGE"], False
+    )
+
+    return render_template(
+        "index.html",
+        title="Explore",
+        posts=posts.items,
+        next_url=url_for("explore", page=posts.next_num) if posts.has_next else None,
+        prev_url=url_for("explore", page=posts.prev_num) if posts.has_prev else None,
+    )
 
 
 @app.route("/login", methods=["GET", "POST"])
