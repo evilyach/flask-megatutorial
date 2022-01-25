d3123517e89c4ae6881252643c8335695d12a383
Ilya Chesalin <ilya.chesalin@gmail.com>
2022-01-25 19:07:06 +0300

    Create a way to submit blog post
    See #1

--- app/routes.py
+++ app/routes.py
@@ -23,9 +23,13 @@ from app.forms import (
     EditProfileForm,
     EmptyForm,
     LoginForm,
+    PostForm,
     RegistrationForm,
 )
-from app.models import User
+from app.models import (
+    Post,
+    User,
+)
 
 
 @app.before_request
@@ -35,10 +39,21 @@ def before_request():
         db.session.commit()
 
 
-@app.route("/")
-@app.route("/index")
+@app.route("/", methods=["GET", "POST"])
+@app.route("/index", methods=["GET", "POST"])
 @login_required
 def index():
+    form = PostForm()
+
+    if form.validate_on_submit():
+        post = Post(body=form.post.data, author=current_user)
+
+        db.session.add(post)
+        db.session.commit()
+
+        flash("Your post is now live!")
+        return redirect(url_for("index"))
+
     posts = [
         {
             "author": {"username": "Alexey"},
@@ -52,6 +67,7 @@ def index():
     return render_template(
         "index.html",
         title="Home",
+        form=form,
         posts=posts
     )
 
