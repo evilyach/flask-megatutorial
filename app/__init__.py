0d8f5ff9ea3cc1059f63e54ef6d87ca3a97b7f1a
Ilya Chesalin <ilya.chesalin@gmail.com>
2022-02-19 16:32:11 +0300

    Use moment.js to manage time and dates
    See #12

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_migrate import Migrate
+from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 
 from config import Config
@@ -18,5 +19,6 @@ bootstrap = Bootstrap(app)
 login = LoginManager(app)
 login.login_view = "login"
 mail = Mail(app)
+moment = Moment(app)
 
 from app import routes, models, errors, log
